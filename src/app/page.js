d187e217da482c454e3884700cd557ea96e0bd31
federico <federicoameratte@gmail.com>
2023-12-21 23:35:44 -0300

    a

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,22 @@
 import { Suspense } from "react";
 import HomeSelect from "../components/home/HomeSelect";
-import Loading from './Loading';
+import Loading from './Loading'
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import { db } from '../firebase/config';
+import { db } from '../firebase/config'; // Replace with the correct path
 
-export async function getServerSideProps() {
+
+const getProducts = async() => {
   const productsRef = collection(db, 'Productos');
-  const q = query(productsRef, where('id', 'in', [1, 2, 3]));
+  const q = query(productsRef, where('id', 'in', [1, 2, 3])); // Fetch limited data for product IDs 1, 2, and 3
   const querySnapshot = await getDocs(q);
 
   const limitedDataFromDb = querySnapshot.docs.map(doc => doc.data());
-  return {
-    props: {
-      limitedData: limitedDataFromDb,
-    },
-  };
+  return limitedDataFromDb;
 }
 
-export default function Home({ limitedData }) {
+export default async function Home() {
+  const limitedData = await getProducts();
+
   return (
     <div className="mb-auto flex flex-col items-center justify-center">
       <h1 className="title mb-5 leading-none text-[#ede0d0] text-[9rem]">
@@ -25,6 +24,7 @@ export default function Home({ limitedData }) {
       </h1>
       <Suspense fallback={<Loading />}>
         <HomeSelect limitedData={limitedData} />
+
       </Suspense>
     </div>
   );
